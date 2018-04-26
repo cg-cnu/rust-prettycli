51c8d6fbb208e7227e983235a7733838411ce7f5
cg-cnu <sreenivas9alapati@gmail.com>
2018-04-26 13:05:15 +0530

    initial commit

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,31 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+pub fn error(message: &str) {
+    println!("🤦  \x1B[1;31mERROR :\x1B[0m \x1B[31m{}\x1B[0m", message);
+}
+
+pub fn info(message: &str) {
+    println!("💁  \x1B[1;32mINFO :\x1B[0m \x1B[32m{}\x1B[0m", message);
+}
+
+pub fn warn(message: &str) {
+    println!("⚠️  \x1B[1;33mWARN :\x1B[0m \x1B[33m{}\x1B[0m", message);
+}
+
+pub fn wait(message: &str) {
+    println!("🙄  \x1B[1;34mWAIT :\x1B[0m \x1B[34m{}\x1B[0m", message);
+}
+
+pub fn critical(message: &str) {
+    println!("🚨  \x1B[1;35mCRITICAL :\x1B[0m \x1B[35m{}\x1B[0m", message);
+}
+
+pub fn command(message: &str) {
+    println!("⚡  \x1B[1;35mCMD :\x1B[0m \x1B[35m{}\x1B[0m", message);
+}
+
+pub fn link(message: &str) {
+    println!("🔗  \x1B[4;34mLINK :\x1B[0m \x1B[4;34m{}\x1B[0m", message);
+}
+
+pub fn ignore(message: &str) {
+    println!("😴  \x1B[2;37mIgnore \x1B[2;37m{}\x1B[0m", message);
 }
